f31bc943e01a53afef72203d18df8437ab3e7c69
SIRINEK-PC\sirinek <sirinek@wisc.edu>
2017-03-09 20:16:39 -0600

    Base UI for Setup and main view

---
+++ app/src/main/java/com/cs506/accountable/Setup_7_Activity.java
@@ -0,0 +1,31 @@
+package com.cs506.accountable;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.design.widget.FloatingActionButton;
+        import android.support.design.widget.Snackbar;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.Toolbar;
+        import android.view.View;
+
+public class Setup_7_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_7_);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Accountable Setup");
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /*
+    Save Goal and move to next Activity
+     */
+    public void moveNext(View view) {
+        Intent intent = new Intent(this, Main_Activity.class);
+        startActivity(intent);
+    }
+}
