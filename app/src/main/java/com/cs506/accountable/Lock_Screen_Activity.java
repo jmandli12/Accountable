872eedd8a4db0a908832698018ce979d25d02391
sirinek <sirinek@wisc.edu>
2017-04-01 16:09:34 -0500

    Start Screen Logic working(I think)

--- app/src/main/java/com/cs506/accountable/Lock_Screen_Activity.java
+++ app/src/main/java/com/cs506/accountable/Lock_Screen_Activity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cs506.accountable.dto.User;
 import com.cs506.accountable.sqlite.DataSource;
@@ -20,36 +21,39 @@ import java.util.List;
 public class Lock_Screen_Activity extends AppCompatActivity {
 
     DataSource ds;
+    int pin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         //Check to see if it is users first time running application
         boolean firstTime = false; //TODO: get this value from the database
-        boolean hasPin = true; //TODO: get this value from the database
+        boolean hasPin = false; //TODO: get this value from the database
 
-        try{
-            SQLiteDatabase checkDB = SQLiteDatabase.openDatabase("/data/data/com.cs506.accountable/databases/accountable.db",null,SQLiteDatabase.OPEN_READONLY);
+        try {
+            SQLiteDatabase checkDB = SQLiteDatabase.openDatabase("/data/data/com.cs506.accountable/databases/accountable.db", null, SQLiteDatabase.OPEN_READONLY);
             checkDB.close();
-        }catch (Exception e){
+        } catch (Exception e) {
+            firstTime = true;
+        }
+        ds = new DataSource(Lock_Screen_Activity.this);
+        ds.open();
+        if(ds.retrieveAll("user").size()==0){
             firstTime=true;
         }
 
-        if(firstTime){
+        if (firstTime) {
             Intent intent = new Intent(this, WelcomeActivity.class);
             startActivity(intent);
-        }
-        else{
-            //ds = new DataSource(Lock_Screen_Activity.this);
-            //ds.open();
+        } else {
+            List<Object> obj = ds.retrieveAll("user");
+            User user = (User) obj.get(0);
+            if (user.getHasPin() == 1) {
+                hasPin = true;
+                pin = user.getPin();
+            }
 
-            //List<Object> obj = ds.retrieveAll("user");
-            //User user = (User) obj.get(0);
-            //if(user.getHasPin() == 0){
-               // hasPin=false;
-           // }
-
-            if(!hasPin){
+            if (!hasPin) {
                 Intent intent = new Intent(this, Main_Activity.class);
                 startActivity(intent);
             }
@@ -69,11 +73,18 @@ public class Lock_Screen_Activity extends AppCompatActivity {
     public void buttonClickHandler(View view) {
 
         EditText et = (EditText) findViewById(R.id.passwordPin);
-        String pin = et.getText().toString();
+        int pin1 = Integer.parseInt(et.getText().toString());
 
 
-        //If the pins match then go to main view
-        Intent intent = new Intent(this, Main_Activity.class);
-        startActivity(intent);
+        if(pin == pin1){
+            //If the pins match then go to main view
+            Intent intent = new Intent(this, Main_Activity.class);
+            startActivity(intent);
+        }
+        else{
+            et.setText("");
+            Toast.makeText(this, "Pins do not Match.  Try Again", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
